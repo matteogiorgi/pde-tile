18972951de9d73803ce39fc55e9eefc6492b78f4
Matteo <mattegiorgi@gmail.com>
2024-06-28 19:01:28 +0200

    update

--- qtile/config.py
+++ qtile/config.py
@@ -156,7 +156,7 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []
-follow_mouse_focus = False
+follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = True
