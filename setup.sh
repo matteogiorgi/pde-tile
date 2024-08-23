2c45aba0e42910155367a15026494abe15625f64
Matteo <mattegiorgi@gmail.com>
2024-08-23 08:33:46 +0200

    add gparted

--- setup.sh
+++ setup.sh
@@ -71,7 +71,7 @@ SCRIPTPATH="$( cd "$(command dirname "$0")" ; pwd -P )" || exit 1
 command sudo apt-get update && sudo apt-get upgrade -qq -y || error-echo "syncing repos"
 command sudo apt-get install -qq -y python3 python3-pip fonts-firacode network-manager diodon flameshot \
       lxpolkit lxterminal lxappearance pavucontrol arandr pcmanfm xarchiver xournalpp firefox nitrogen \
-      xsettingsd libwacom9 xserver-xorg-input-wacom adwaita-icon-theme-full || error-echo "installing from apt"
+      gparted xsettingsd libwacom9 xserver-xorg-input-wacom adwaita-icon-theme-full || error-echo "installing from apt"
 command pip3 install qtile || error-echo "installing from pip"
 # ---
 store-conf
