23c5ca253017103f36ab13af6bfbbdaf1ca10834
Matteo <mattegiorgi@gmail.com>
2024-08-27 19:31:09 +0200

    updates

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -11,10 +11,32 @@
 
 
 
+### Defaults
+############
+
 command lxpolkit &
 command nm-applet &
 command diodon &
 command flameshot &
 command pcmanfm -d &
+command nitrogen --restore &
 command ulauncher --hide-window --no-window-shadow &
 command setxkbmap us -option "caps:swapescape"
+
+
+
+
+### Custom
+##########
+
+XRANDRID1="HDMI-1"
+XRANDRID2="eDP-1"
+WIDTHxHEIGHT="1024x768"
+POSITION="--right-of"
+ROTATION="half"
+TOUCHPADID=$(xinput --list --name-only | grep -Eo '.*'"[Tt]ouch[Pp]ad"'.*' | head -n 1)
+WACOMID=$(xsetwacom --list devices | grep -Eo '.*'"[Ww][Aa][Cc][Oo][Mm]"'.*' | head -n 1)
+# ---
+command xinput disable "$TOUCHPADID"
+command xrandr --output "$XRANDRID2" --mode "$WIDTHxHEIGHT" "$POSITION" "$XRANDRID1"
+command xsetwacom set "$WACOMID" MapToOutput "$XRANDRID2" rotate "$ROTATION"
