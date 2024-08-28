b502f1fdf1231a6775168ebdac3b626280fe05f4
Matteo <mattegiorgi@gmail.com>
2024-08-28 14:41:07 +0200

    automated startup

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -29,14 +29,17 @@ command setxkbmap us -option "caps:swapescape"
 ### Custom
 ##########
 
-XRANDRID1="HDMI-1"
-XRANDRID2="eDP-1"
-WIDTHxHEIGHT="1024x768"
-POSITION="--right-of"
-ROTATION="half"
-TOUCHPADID=$(xinput --list --name-only | grep -Eo '.*'"[Tt]ouch[Pp]ad"'.*' | head -n 1)
-WACOMID=$(xsetwacom --list devices | grep -Eo '.*'"[Ww][Aa][Cc][Oo][Mm]"'.*' | head -n 1)
+SCREEN1_ID="eDP-1"
+SCREEN2_ID="HDMI-1"
+SCREEN2_POSITION="--right-of"
+SCREEN2_RATIO="1024x768"
+TOUCH_ID=$(xinput --list --name-only | grep -Eo '.*'"[Tt]ouch[Pp]ad"'.*' | head -n 1)
+WACOM_ID=$(xinput --list --name-only | grep -Eo '.*'"[Ww][Aa][Cc][Oo][Mm].*[Ss]tylus"'.*' | head -n 1)
+WACOM_ROTATION="half"
 # ---
-command xinput disable "$TOUCHPADID"
-command xrandr --output "$XRANDRID2" --mode "$WIDTHxHEIGHT" "$POSITION" "$XRANDRID1"
-command xsetwacom set "$WACOMID" MapToOutput "$XRANDRID2" rotate "$ROTATION"
+command xinput disable "$TOUCH_ID"
+command xsetwacom set "$WACOM_ID" rotate "$WACOM_ROTATION"
+if xrandr | grep "$SCREEN1_ID" | grep "$SCREEN2_ID"; then
+    command xrandr --output "$SCREEN2_ID" --mode "$SCREEN2_RATIO" "$SCREEN2_POSITION" "$SCREEN1_ID"
+    command xsetwacom set "$WACOM_ID" MapToOutput "$SCREEN2_ID" rotate "$WACOM_ROTATION"
+fi
